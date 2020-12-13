a2d10f13bbedffb0ff72de26371f4e474de995da
xroah <396069583@qq.com>
2020-12-13 22:30:08 +0800

    unit test configuration

---
+++ karma.conf.js
@@ -0,0 +1,85 @@
+module.exports = function (config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: ".",
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ["jasmine"],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      "lib/**/*.js",
+      "tests/index.spec.js"
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      "lib/**/*.js": ["webpack", "coverage"],
+      "tests/index.spec.js": ["webpack"]
+    },
+
+    webpack: {
+      mode: "development",
+      stats: "errors-only",
+      module: {
+        rules: [{
+          test: /\.js$/,
+          use: "@jsdevtools/coverage-istanbul-loader",
+          exclude: /tests|node_modules/
+        }]
+      }
+    },
+
+    // test results reporter to use
+    // possible values: "dots", "progress"
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ["progress", "coverage"],
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type: "html",
+      dir: "coverage/"
+    },
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ["ChromeHeadless"],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
